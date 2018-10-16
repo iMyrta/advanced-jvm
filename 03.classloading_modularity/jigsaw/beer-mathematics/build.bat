857a09f9bf86596fb19dcb0f55794584422e6de6
Ivaylo Pashov <Ivaylo_Pashov@epam.com>
2018-10-16 12:21:58 +0300

    03. Classloading and Modularity Removing not needed files.

--- 03.classloading_modularity/jigsaw/beer-mathematics/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rmdir build /s /q
-mkdir build
-"C:\Program Files\Java\jdk-10.0.1\bin\javac" -d build --module-path ../assembly src/module-info.java src/beer/mathematics/BeerParameter.java src/beer/mathematics/BeerMathResult.java
-cd build
-"C:\Program Files\Java\jdk-10.0.1\bin\jar" cf beer-mathematics.jar *
-cd ..
