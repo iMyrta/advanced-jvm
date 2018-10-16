66a9d897dbba7a9c7a810f92c9dffc8cc580c4ad
Daniel Darakchiev <danieldarakchiev@hotmail.com>
2018-10-16 14:04:21 +0300

    update script module paths

--- 03.classloading_modularity/jigsaw/demo-scripts/audit.bat
+++ 03.classloading_modularity/jigsaw/demo-scripts/audit.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jdk-10.0.1\bin\java" --module-path ..\assembly --add-modules ipa.brewer -m audit/audit.AuditMain
+"C:\Program Files\Java\jdk-10.0.1\bin\java" --module-path ..\out\production --add-modules ipa.brewer -m audit/audit.AuditMain
