70beeda947ac8ca3c84bf4b773a87b0225cc59aa
Daniel Darakchiev <danieldarakchiev@hotmail.com>
2018-10-31 12:29:46 +0200

    add flyweight demo

--- 06.memory_management/flyweight/src/main/java/Flyweight.java
+++ 06.memory_management/flyweight/src/main/java/Flyweight.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Flyweight {
 
-    private static final boolean USE_FLYWEIGHT = false;
+    private static final boolean USE_FLYWEIGHT = true;
     private static final int GARBAGE_SIZE = 50000;
     private static final String[] SHAPES = { "Bottle", "Can", "Bag" };
     private static final List<Garbage> GARBAGE_BIN = new ArrayList<>(GARBAGE_SIZE);
@@ -13,14 +13,21 @@ public class Flyweight {
         Runtime runtime = Runtime.getRuntime();
 
         for (int i=0; i < GARBAGE_SIZE; i++) {
-            Garbage garbage = USE_FLYWEIGHT ? GarbageFactory.getPlasticGarbage((getRandomShape())) : new PlasticGarbage((getRandomShape()));
+            Garbage garbage = USE_FLYWEIGHT ? GarbageFactory.getPlasticGarbage((getPredictableShape())) : new PlasticGarbage((getPredictableShape()));
             garbage.generate();
             GARBAGE_BIN.add(garbage);
         }
 
         final long memoryUsed = (runtime.totalMemory() - runtime.freeMemory()) / 1024 / 1024;
 
+        System.out.println();
         System.out.println((USE_FLYWEIGHT ? "" : "Non-") + "Flyweight memory used: " + memoryUsed + " mb");
+    }
+
+    private static int shapeCount;
+
+    private static String getPredictableShape() {
+        return SHAPES[(int)shapeCount++ % SHAPES.length];
     }
 
     private static String getRandomShape() {
